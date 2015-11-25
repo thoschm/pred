15e53132c95be643b5d850af1da7334654560067
Thomas Schmiedel <thomas.schmiedel@tu-ilmenau.de>
2015-11-25 13:15:48 +0100

    init

---
+++ include/Predictor.h
@@ -0,0 +1,256 @@
+
+#ifndef _PREDICTOR_H_
+#define _PREDICTOR_H_
+
+/////////////////////////////////
+// INCLUDES
+/////////////////////////////////
+#include "PSO.h"
+#include <iostream>
+#include <cmath>
+#include <vector>
+#include <string.h>
+#include <float.h>
+#include <limits.h>
+
+
+/////////////////////////////////
+// TYPEDEFS
+/////////////////////////////////
+typedef unsigned int uint;
+
+
+/////////////////////////////////
+// NAMESPACE
+/////////////////////////////////
+namespace Predictor
+{
+
+
+/////////////////////////////////
+// KERNEL
+/////////////////////////////////
+template <typename NumericalType>
+struct MuSigmaScale
+{
+    NumericalType mu, sigma, scale;
+};
+
+template <typename NumericalType, int Window, int Nodes>
+struct Kernel
+{
+    MuSigmaScale<NumericalType> data[Window * Nodes];
+};
+
+
+/////////////////////////////////
+// KERNEL MANAGER
+/////////////////////////////////
+template <typename NumericalType, int Window, int Nodes>
+class KernelOperation
+{
+    KernelOperation();
+    KernelOperation(const KernelOperation &other);
+    KernelOperation &operator=(const KernelOperation &other);
+
+public:
+    // print
+    static void print(const Kernel<NumericalType, Window, Nodes> &krnl)
+    {
+        for (uint i = 0; i < Window; ++i)
+        {
+            std::cerr << i << ": ";
+            for (uint k = 0; k < Nodes; ++k)
+            {
+                const uint idx = i * Nodes + k;
+                std::cerr << "(" << krnl.data[idx].mu
+                          << "," << krnl.data[idx].sigma
+                          << "," << krnl.data[idx].scale << ") ";
+            }
+            std::cerr << std::endl;
+        }
+    }
+
+    // reset kernel
+    static void reset(Kernel<NumericalType, Window, Nodes> &krnl,
+                      const NumericalType mu,
+                      const NumericalType sigma,
+                      const NumericalType scale)
+    {
+        for (uint i = 0; i < Window * Nodes; ++i)
+        {
+            krnl.data[i].mu = mu;
+            krnl.data[i].sigma = sigma;
+            krnl.data[i].scale = scale;
+        }
+    }
+
+    // compute simple gaussian with custom scaling
+    static NumericalType gaussian(const NumericalType mu,
+                                  const NumericalType sigma,
+                                  const NumericalType scale,
+                                  const NumericalType value)
+    {
+        NumericalType d = value - mu;
+        //return scale * std::exp((NumericalType)-0.5 * d * d / (sigma * sigma));
+        return scale / ((NumericalType)1.0 + sigma * d * d);
+    }
+
+    // compute kernel response to an input vector
+    static NumericalType response(const Kernel<NumericalType, Window, Nodes> &krnl,
+                                  const std::vector<NumericalType> &data,
+                                  const uint startAt)
+    {
+        NumericalType sum = (NumericalType)0.0;
+        for (uint i = 0; i < Window; ++i)
+        {
+            for (uint k = 0; k < Nodes; ++k)
+            {
+                const uint idx = i * Nodes + k;
+                sum += gaussian(krnl.data[idx].mu,
+                                krnl.data[idx].sigma,
+                                krnl.data[idx].scale,
+                                data[startAt + i]);
+            }
+        }
+        return sum / (NumericalType)(Window * Nodes);
+    }
+
+    // compute convolution error of input sequence
+    static NumericalType convolution(const Kernel<NumericalType, Window, Nodes> &krnl,
+                                     const std::vector<NumericalType> &data,
+                                     const NumericalType targetValue,
+                                     const NumericalType targetSigma,
+                                     const uint ahead = 1)
+    {
+        const uint limit = data.size() - Window - ahead;
+        NumericalType error = (NumericalType)0.0;
+        for (uint i = 0; i <= limit; ++i)
+        {
+            const NumericalType teacher = gaussian(targetValue, targetSigma,
+                                          (NumericalType)1.0, data[i + Window + ahead - 1]),
+                                resp    = response(krnl, data, i);
+            const NumericalType tmp = resp - teacher;
+            error += tmp * tmp;
+        }
+        return error / (NumericalType)(limit + 1u);
+    }
+
+    // compute convolution error of input sequence
+    static void applyKernel(const Kernel<NumericalType, Window, Nodes> &krnl,
+                            const std::vector<NumericalType> &data,
+                            std::vector<NumericalType> *out,
+                            const uint ahead = 1)
+    {
+        out->clear();
+        out->resize(data.size(), (NumericalType)0.0);
+        const uint limit = data.size() - Window - ahead;
+        for (uint i = 0; i <= limit; ++i)
+        {
+            out->at(i + Window + ahead - 1) = response(krnl, data, i);
+        }
+    }
+};
+
+
+/////////////////////////////////
+// KERNEL MANAGER
+/////////////////////////////////
+template <typename NumericalType, int Window, int Nodes>
+class KernelOptimizer
+{
+    KernelOptimizer();
+    KernelOptimizer(const KernelOptimizer &other);
+    KernelOptimizer &operator=(const KernelOptimizer &other);
+
+public:
+    struct Payload
+    {
+        NumericalType minSigma;
+        const std::vector<NumericalType> *data;
+        NumericalType targetValue,
+                      targetSigma;
+        uint targetAhead;
+    };
+
+
+    static NumericalType optimize(Kernel<NumericalType, Window, Nodes> &result,
+                                  const std::vector<NumericalType> &data,
+                                  const NumericalType targetValue,
+                                  const NumericalType targetSigma,
+                                  const uint targetAhead,
+                                  const NumericalType lowerLimit,
+                                  const NumericalType upperLimit,
+                                  const uint particleCount,
+                                  const NumericalType breakScore)
+    {
+        Payload pl;
+        pl.data = &data;
+        pl.targetValue = targetValue;
+        pl.targetSigma = targetSigma;
+        pl.targetAhead = targetAhead;
+        pl.minSigma = 0.00001f;
+
+        PSO<NumericalType, Window * Nodes * 3> pso(particleCount, scoreFunc, (const void *)&pl);
+        pso.init(lowerLimit, upperLimit);
+        NumericalType s;
+        while ((s = pso.step()) > breakScore)
+        {
+            std::cout << s << std::endl;
+        }
+        const NumericalType *values = pso.getBest();
+
+        uint cnt = 0;
+        for (uint i = 0; i < Window * Nodes; ++i)
+        {
+            result.data[i].mu    = values[cnt++];
+            result.data[i].sigma = values[cnt++];
+            result.data[i].scale = values[cnt++];
+        }
+
+        return pso.getScore();
+    }
+
+
+    static NumericalType scoreFunc(const NumericalType *values, const uint dim, const void *payload)
+    {
+        // check dim
+        if (dim != Window * Nodes * 3)
+        {
+            std::cerr << "score: invalid dimensions\n";
+            return (NumericalType)FLT_MAX;
+        }
+
+        // get payload
+        const Payload *pl = (const Payload *)payload;
+
+        // assemble kernel
+        Kernel<NumericalType, Window, Nodes> krnl;
+        uint cnt = 0;
+        for (uint i = 0; i < Window * Nodes; ++i)
+        {
+            krnl.data[i].mu    = values[cnt++];
+            krnl.data[i].sigma = values[cnt++];
+            krnl.data[i].scale = values[cnt++];
+
+            // reject if sigma too small
+            if (krnl.data[i].sigma < pl->minSigma)
+            {
+                return (NumericalType)FLT_MAX;
+            }
+        }
+
+        // compute cost
+        return KernelOperation<NumericalType, Window, Nodes>::convolution(krnl,
+                                                                          *(pl->data),
+                                                                          pl->targetValue,
+                                                                          pl->targetSigma,
+                                                                          pl->targetAhead);
+    }
+
+
+};
+
+}
+
+#endif
---
+++ src/demo.cpp
@@ -0,0 +1,106 @@
+
+
+#include <Predictor.h>
+#include <fstream>
+#include <PSO.h>
+
+
+using namespace Predictor;
+
+
+#define WINDOW 50
+#define NODES  2
+#define TARGET 1.0
+#define TSIGMA 1.0
+
+#define RAD(_a) ((_a) * M_PI / 180.0)
+
+float func(uint i)
+{
+    return std::sin(2.0 * RAD(i)) + std::sin(13.0 * RAD(i)) + std::cos(5.0 * RAD(i)) +
+           std::sin(0.5 * RAD(i + 10)) + std::sin(0.2 * RAD(i + 50)) + std::cos(2.0 * RAD(i + 100)) +
+           std::sin(1.5 * RAD(i + 250)) * std::sin(0.5 * RAD(i + 150));
+}
+
+
+float scoreFunc(const float *array, const uint dim, const void *payload)
+{
+    if (array[0] < 5.0) return FLT_MAX;
+    if (array[1] < 5.0) return FLT_MAX;
+    float sum = 0.0f;
+    for (uint i = 0; i < dim; ++i)
+    {
+        sum += array[i] * array[i];
+    }
+    return sum;
+}
+
+
+int main(int argc, char **argv)
+{
+
+
+    std::vector<float> data, out, err;
+    std::ofstream ofs;
+    ofs.open("sine.txt");
+    for (uint i = 0; i < 1000u; ++i)
+    {
+        //data.push_back(1.0f);
+        data.push_back(func(i));
+        ofs << i << " " << data.back() << std::endl;
+    }
+    /*for (uint i = 1000; i < 2000u; ++i)
+    {
+        data.push_back(0.0f);
+        //data.push_back(std::sin(i * M_PI / 180.0f));
+        ofs << i << " " << data.back() << std::endl;
+    }
+    for (uint i = 2000; i < 3000u; ++i)
+    {
+        data.push_back(3.0f);
+        //data.push_back(std::sin(i * M_PI / 180.0f));
+        ofs << i << " " << data.back() << std::endl;
+    }
+    for (uint i = 3000; i < 4000u; ++i)
+    {
+        data.push_back(5.0f);
+        //data.push_back(std::sin(i * M_PI / 180.0f));
+        ofs << i << " " << data.back() << std::endl;
+    }*/
+    ofs.close();
+
+
+    Kernel<float, WINDOW, NODES> krnl;
+    KernelOptimizer<float, WINDOW, NODES>::optimize(krnl, data, TARGET, TSIGMA, 100u, 0.0f, 1.0f, 100u, 0.02f);
+    KernelOperation<float, WINDOW, NODES>::applyKernel(krnl, data, &out, 100u);
+    KernelOperation<float, WINDOW, NODES>::print(krnl);
+    ofs.open("out.txt");
+    for (uint i = 0; i < out.size(); ++i)
+    {
+        ofs << i << " " << out[i] << std::endl;
+    }
+    ofs.close();
+
+    /*
+    ofs.open("kernel.txt");
+    for (uint i = 0; i < WINDOW; ++i)
+    {
+       ofs << i << " " << krnl.data[i].mu << std::endl;
+    }
+    ofs.close();
+    ofs.open("scale.txt");
+    for (uint i = 0; i < KERNEL_SIZE; ++i)
+    {
+       ofs << i << " " << krnl.data[i].scale << std::endl;
+    }
+    ofs.close();
+    ofs.open("sigma.txt");
+    for (uint i = 0; i < KERNEL_SIZE; ++i)
+    {
+       ofs << i << " " << krnl.data[i].sigma << std::endl;
+    }
+    ofs.close();*/
+
+
+    return 0;
+}
