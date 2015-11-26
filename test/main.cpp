94b847d1a777607056d843e9779b3c8dc8834ed9
Thomas Schmiedel <thomas.schmiedel@tu-ilmenau.de>
2015-11-26 16:32:58 +0100

    blubb

---
+++ test/main.cpp
@@ -0,0 +1,40 @@
+
+
+#include <fstream>
+#include <iostream>
+
+typedef unsigned int uint;
+
+int main(int argc, char **argv)
+{
+    const uint interval = 3600u; // seconds
+    uint nexttime = 0;
+    float lastPrice = 0.0;
+    std::ifstream ifs;
+    ifs.open("price.txt", std::ios::in);
+    std::ofstream ofs;
+    ofs.open("chart.txt", std::ios::out);
+    for ( ; ; )
+    {
+        uint stamp;
+        float price, vol;
+        ifs >> stamp
+            >> price
+            >> vol;
+        if (ifs.eof())
+        {
+            break;
+        }       
+        
+        if (stamp >= nexttime)
+        {
+            nexttime = stamp + interval;
+            ofs << lastPrice << std::endl;
+            //std::cerr << stamp << " " << lastPrice << std::endl;        
+        }
+        lastPrice = price;        
+    }
+    ifs.close();
+    ofs.close();
+    return 0;
+}
